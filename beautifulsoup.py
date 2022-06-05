83b5a3de3f647a6d8e699c5ceb2f67d0a303308b
Shin_Seong_Cheol <46277114+ShinSeongCheol@users.noreply.github.com>
2022-06-05 23:47:33 +0900

    Create beautifulsoup.py

---
+++ beautifulsoup.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup as bs
+import requests as rq
+import pandas as pd
+res = rq.get('https://movie.naver.com/movie/running/current.nhn')
+soup = bs(res.content, 'html.parser')
+movieTitle = soup.select('#content > div.article > div > div.lst_wrap > ul > li > dl > dt > a')
+movieRating = soup.select('#content > div.article > div > div.lst_wrap > ul > li > dl > dd.star > dl > dd > div > a > span.num') 
+movieVote = soup.select('#content > div.article > div > div.lst_wrap > ul > li > dl > dd.star > dl > dd > div > a > span.num2 > em')
+
+movie=[]
+rating=[]
+numOfParti = []
+
+for title in movieTitle :
+    movie.append(title.text.strip())
+for rate in movieRating :
+    rating.append(rate.get_text())
+for vote in movieVote : 
+    numOfParti.append(vote.get_text())
+
+
+naverMovie = pd.DataFrame({'제목': movie, '평점' : rating, '참여인원' : numOfParti})
+naverMovie
